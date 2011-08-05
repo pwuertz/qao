f76bf483b1468c62ef55930deb17a40164b242ea
(no author) <(no author)@be6eae23-869e-4e2f-a80b-449878f96085>
2011-08-05 15:59:28 +0000

    Agilent33500 added
    git-svn-id: http://qao/svn@244 be6eae23-869e-4e2f-a80b-449878f96085

--- src/qao/devices/funcgen.py
+++ src/qao/devices/funcgen.py
@@ -3,7 +3,7 @@
 
 import socket, numpy, sys
 
-class AgilentFuncGen:
+class Agilent33220:
     __BUFSIZE__ = 1024
     __TIMEOUT__ = 4.0
     __DEFPORT__ = 5025
@@ -225,3 +225,244 @@ class AgilentFuncGen:
     def applyArb(self, name = "volatile"):
         self.send("func:user " + name)
         self.send("func:shape user")
+
+AgilentFuncGen = Agilent33220
+
+class Agilent33500:
+    __BUFSIZE__ = 1024
+    __TIMEOUT__ = 4.0
+    __DEFPORT__ = 5025
+    
+    def __init__(self, host, debug=False):
+        self.__sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.__sock.setblocking(True)
+        self.__sock.settimeout(self.__TIMEOUT__)
+        self._sock = self.__sock
+        self.__host = host
+        self.__debug = debug
+    
+    def setHostname(self, hostname):
+        self.__host = hostname
+        
+    def connect(self):
+        self.__sock.connect((self.__host, self.__DEFPORT__))
+        
+    def disconnect(self):
+        self.__sock.close()
+        
+    def errorcheck(self):
+        self.__sock.sendall("syst:err?\n")
+        error = self.__sock.recv(self.__BUFSIZE__).rstrip()
+        err_id, err_str = error.split(",")
+        if int(err_id) != 0:
+            raise Exception(err_str)
+        
+    def send(self, command):
+        self.__sock.sendall(command + "\n")
+        if self.__debug: self.errorcheck()
+        
+    def query(self, command):
+        self.__sock.send(command + "\n")
+        result = self.__sock.recv(self.__BUFSIZE__).rstrip()
+        if self.__debug: self.errorcheck()
+        return result
+    
+    def waitForOPC(self):
+        if int(self.query("*opc?")) != 1: raise Exception("OPC did not return True")
+    
+    def reset(self):
+        self.send("*rst")
+
+    def abort(self):
+        self.send("abor")
+    
+    def voltHigh(self, value=None, unit="V", channel=1):
+        if value == None:
+            return float(self.query("sour%d:volt:high?" % channel))
+        else:
+            self.send("sour%d:volt:high %f %s" % (channel, float(value), unit))
+            
+    def voltLow(self, value=None, unit="V", channel=1):
+        if value == None:
+            return float(self.query("sour%d:volt:low?" % channel))
+        else:
+            self.send("sour%d:volt:low %f %s" % (channel, float(value), unit))
+            
+    def voltAmplitude(self, value=None, unit="V", channel=1):
+        if value == None:
+            return float(self.query("sour%d:volt:amplitude?" % channel))
+        else:
+            self.send("sour%d:volt:amplitude %f %s" % (channel, float(value), unit))
+            
+    def voltOffset(self, value=None, unit="V", channel=1):
+        if value == None:
+            return float(self.query("sour%d:volt:offs?" % channel))
+        else:
+            self.send("sour%d:volt:offs %f %s" % (channel, float(value), unit))
+            
+    def frequency(self, value=None, channel=1):
+        if value == None:
+            return float(self.query("sour%d:freq?" % channel))
+        else:
+            self.send("sour%d:freq %.3f" % (channel, float(value)))
+            
+    def period(self, value=None, unit="s", channel=1):
+        if value == None:
+            return float(self.query("sour%d:func:puls:per?" % channel))
+        else:
+            self.send("sour%d:func:puls:per %.9f %s" % (channel, float(value), unit))
+            
+    def setInverted(self, inverted, channel=1):
+        if inverted:
+            self.send("outp%d:pol inv" % channel)
+        else:
+            self.send("outp%d:pol norm" % channel)
+            
+    def isInverted(self, channel=1):
+        return self.query("outp%d:polarity?" % channel) == "INV"
+
+    def setTriggeredBurst(self, ncycles = 1, channel=1):
+        """
+        Wait for an external trigger and run the configured
+        waveform for ncycles (burst mode).
+        """
+        self.send('sour%d:burs:mode trig' % channel)
+        self.send('sour%d:burs:ncyc %d' % (channel, ncycles))
+        self.send('trig%d:sour ext' % channel)
+        self.send('sour%d:burs:stat on' % channel)
+        
+    def setOutputLoad(self, load = 50, channel=1):
+        """
+        Set the output termination to the specified load (ohms).
+        The default is 50 ohms.
+        """
+        self.send('outp%d:load %d' % (channel, load))
+        
+    def setOutputHigh(self, channel=1):
+        """
+        Set the output termination to high impedance.
+        """
+        self.send('outp%d:load inf' % channel)
+        
+    def setEnabled(self, enabled, channel=1):
+        """
+        Enable or disable the output of the function generator.
+        """
+        if enabled:
+            self.send('outp%d on' % channel)
+        else:
+            self.send('outp%d off' % channel)
+
+    def applyRamp(self, freq, amp, off, channel=1):
+        """
+        Apply a ramp with specified frequency, amplitude and offset.
+        """
+        self.send("sour%d:appl:ramp %.3f, %.3f, %.3f" % (channel, freq, amp, off))
+        
+    def applyPositiveRamp(self, freq, amp, off, channel=1):
+        """
+        Apply a positive linear ramp with specified
+        frequency, amplitude and offset.
+        """
+        self.applyRamp(freq, amp, off, channel = channel)
+        self.setInverted(True, channel = channel)
+        self.send("sour%d:func:ramp:symm 0" % channel)
+        
+    def applyNegativeRamp(self, freq, amp, off, channel=1):
+        """
+        Apply a negative linear ramp with specified
+        frequency, amplitude and offset.
+        """
+        self.applyRamp(freq, amp, off, channel = channel)
+        self.setInverted(False, channel = channel)
+        self.send("sour%d:func:ramp:symm 0" % channel)
+        
+    def applyPositiveRamp2(self, period, high, low, channel=1):
+        """
+        Apply a positive linear ramp with specified
+        period, low level and high level.
+        """
+        self.applyPositiveRamp(1./period, high-low, low + 0.5*(high-low), channel = channel)
+        self.period(period, channel = channel) # 1/period might be inaccurate
+        
+    def applyNegativeRamp2(self, period, high, low, channel=1):
+        """
+        Apply a negative linear ramp with specified
+        period, low level and high level.
+        """
+        self.applyNegativeRamp(1./period, high-low, low + 0.5*(high-low), channel = channel)
+        self.period(period, channel = channel) # 1/period might be inaccurate
+
+    def applyDC(self, volt, channel=1):
+        """
+        Apply a DC voltage.
+        """
+        self.send("sour%d:appl:dc def, def, %.3f" % (channel, volt))
+
+    def clearArb(self, channel=1):
+        """
+        Delete all stored arbitrary waveforms.
+        """
+        self.send("sour%d:data:vol:cle" % channel)
+    
+    def freeArbMem(self, channel=1):
+        return self.query("sour%d:data:vol:free?" % channel) 
+
+    def addArbNormalized(self, data, name, channel=1):
+        """
+        Add an arbitrary waveform. The data must be an array or a list
+        of floats normalized to -1.0,1.0.
+        """
+        # TODO: check free space
+        # map [-1:1] to [-8191:8191]
+        data = numpy.asfarray(data)
+        data = numpy.clip(data, -1.0, 1.0)
+        # convert data to 16bit signed int array
+        data = numpy.round(data * 32767).astype(numpy.int16)
+        datastr = data.tostring()
+        
+        # ieee-488.2 block header
+        datalen = str(len(datastr))
+        header = "#" + str(len(datalen)) + datalen
+                         
+        # set byte order
+        if sys.byteorder == 'little':
+            self.send("form:bord swap")
+        else:
+            self.send("form:bord norm")
+        
+        # upload data
+        self.send("sour%d:data:arb:dac %s, %s%s" % (channel, name, header, datastr))
+        
+    def addArbAbsolute(self, data, name, channel=1, unit = "V"):
+        data = numpy.asfarray(data)
+        
+        # normalize data
+        dmax = data.max()
+        dmin = data.min()
+        amp = dmax-dmin
+        off = dmin + 0.5*amp
+        data_norm = (data-off)/(0.5*amp)
+        
+        # add arb, set voltages
+        self.voltAmplitude(amp, unit)
+        self.voltOffset(off, unit)
+        self.addArbNormalized(data_norm, name=name, channel=channel)
+    
+    def addTriggeredSeq(self, name, arb_names, channel):
+        seqblocks = [name]            
+        for arb_name in arb_names:
+            seqblocks.append("%s,0,onceWaitTrig,maintain,4" % arb_name)
+        
+        seqstr = ",".join(seqblocks)
+        cmd = "sour%d:data:seq #%d%d%s" % (channel, len(str(len(seqstr))), len(seqstr), seqstr)
+        self.send(cmd)
+        
+    def applyArb(self, name, samplerate, channel=1):
+        self.send("sour%d:func:arb %s" % (channel, name))
+        self.send("sour%d:func:arb:srat %e" % (channel, samplerate))
+    
+    def saveState(self, num):
+        self.send("*sav %d" % num)
+    def loadState(self, num):
+        self.send("*rcl %d" % num)
