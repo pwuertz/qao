ca5001c2024a085561154a8bb1aa142cd3e05464
(no author) <(no author)@be6eae23-869e-4e2f-a80b-449878f96085>
2011-10-14 12:50:16 +0000

    git-svn-id: http://qao/svn@341 be6eae23-869e-4e2f-a80b-449878f96085

--- src/qao/analysis/image.py
+++ src/qao/analysis/image.py
@@ -15,14 +15,14 @@ def imageMoments(data, X = None, Y = None):
     Yc = Y - my
     
     # calculate central moments
-    mu11 = (X * Y * data).sum()
-    mu20 = (X**2 * data).sum()
-    mu02 = (Y**2 * data).sum()
-    mu22 = (X**2 * Y**2 * data).sum()
-    mu31 = (X**3 * Y * data).sum()
-    mu13 = (X * Y**3 * data).sum()
-    mu40 = (X**4 * data).sum()
-    mu04 = (Y**4 * data).sum()
+    mu11 = (Xc * Yc * data).sum()
+    mu20 = (Xc**2 * data).sum()
+    mu02 = (Yc**2 * data).sum()
+    mu22 = (Xc**2 * Yc**2 * data).sum()
+    mu31 = (Xc**3 * Yc * data).sum()
+    mu13 = (Xc * Yc**3 * data).sum()
+    mu40 = (Xc**4 * data).sum()
+    mu04 = (Yc**4 * data).sum()
     
     # calculate derived quantities
     sigx = mu20**.5; sigy = mu02**.5
@@ -40,4 +40,30 @@ def imageMoments(data, X = None, Y = None):
     # relative univariate kurtosis difference
     rk = abs(ga40 - ga04) / min(ga40, ga04)
     
-    return dict(gv=gv, b22=b22, rv=rv, rk=rk)
+    return dict(gv=gv, b22=b22, rv=rv, rk=rk, mu11=mu11, mu20=mu20, mu02=mu02)
+
+if __name__ == '__main__':
+    import pylab as p
+    
+    # produce gaussian test distribution
+    n = 400
+    alpha = 60 * np.pi / 180.
+    sigy, sigx = 5., 50.
+    x0, y0 = 200., 200.
+    Y, X = np.ogrid[:400,:400]    
+    Xr = np.sin(alpha) * (Y-y0) + np.cos(alpha) * (X-x0)
+    Yr = -np.sin(alpha) * (X-x0) + np.cos(alpha) * (Y-y0) 
+    d = np.exp(- Xr**2 / (2*sigx**2) - Yr**2 / (2*sigy**2))
+    
+    # measure statistic moments
+    moments = imageMoments(d)
+    mu11, mu20, mu02 = moments["mu11"], moments["mu20"], moments["mu02"]
+    
+    # calculate angle from statistic moments
+    alpha_m = .5*np.arctan2(2.*mu11, (mu20-mu02))
+    print "measured angle %.1fdeg, (orig %.1fdeg)" % (alpha_m*180./np.pi, alpha*180./np.pi)
+    
+    # show image
+    p.imshow(d)
+    p.show()
+    
