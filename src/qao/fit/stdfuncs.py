87ed710842f920a4fcc33929d33a73d2b36b40eb
Thomas Niederprüm <niederp@physik.uni-kl.de>
2012-12-17 18:32:34 +0100

    various fixes

--- src/qao/fit/stdfuncs.py
+++ src/qao/fit/stdfuncs.py
@@ -1,4 +1,5 @@
 from qao.fit.fitter import LevmarFitter, DEFAULT_TYPE_NPY
+import numpy as np
 
 class ExpDecay(LevmarFitter):
     def __init__(self, data):
@@ -13,7 +14,7 @@ class ExpDecay(LevmarFitter):
         x0  = (np.arange(data.size) * data).sum() * (1./dsum)
         var = ((np.arange(data.size)-x0)**2 * data).sum() * (1./dsum)
         # return guess
-        return np.asfarray([dmax, x0, np.sqrt(var)], dtype = DEFAULT_TYPE_NPY)
+        return np.asfarray([amp, x0, 0], dtype = DEFAULT_TYPE_NPY)
     
     def f(self, pars):
         # for given set of pars, calculate the fit function
@@ -24,10 +25,49 @@ class ExpDecay(LevmarFitter):
     def fJ(self, pars):
         # for given set of pars, calculate the fit function...
         A, tau, off = pars
-        x = np.arange(data.size, dtype = DEFAULT_TYPE_NPY)
-        self._f[:] = A * np.exp(-1./2. * 1./sig**2 * (x-x0)**2)
+        x = np.arange(self.data.size, dtype = DEFAULT_TYPE_NPY)
+        self._f[:] = A * np.exp(-x/tau)+off
         
         # ...and the derivatives for the jacobian 
-        self._J[0, :] = -1/tau * self._f[:]
-        self._J[1, :] = 1/tau**2 * self._f[:]
-        self._J[2, :] = -1/tau**3 * self._f[:]   
+        self._J[0, :] = self._f[:]
+        self._J[1, :] = -1/(tau) * self._f[:]
+        self._J[2, :] = 1/(tau**2) * self._f[:]
+        
+if __name__ == "__main__":
+        
+    def expdec(x, pars):
+        A, tau, off = pars        
+        return A * np.exp(-x/tau) + off
+
+
+    n = 250
+    X = np.linspace(0,n,100)
+    
+    pars_org = np.asfarray([10,10,0])
+    
+    data_org = expdec(X,  pars_org)
+        
+    fitter = ExpDecay(data_org)
+    fitter.verbose = False
+    pars_ini = fitter.guess()
+    data_ini = expdec(X, pars_ini)
+    pars_fit = fitter.fit(pars_ini)
+    print pars_fit
+    data_fit = expdec(X, pars_fit)
+    
+    """
+    #print pars_fit
+    print "alpha: %.2f"%(fitter.getFitParsDict()['alpha']*180/np.pi)
+    print "sx: %.2f"%fitter.getFitParsDict()['s_x']
+    print "sy: %.2f"%fitter.getFitParsDict()['s_y']
+    """
+    
+    import pylab as p
+    
+    p.plot(data_org,label="orig")
+       
+    p.plot(data_ini,label="init")
+    
+    p.plot(data_fit,label="fit")
+    p.legend()
+    p.show()
