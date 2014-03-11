9ee44dd6cad64d4605d5ceb3bcf349506ed33d6d
Peter Würtz <pwuertz@gmail.com>
2014-03-11 14:46:13 +0100

    Use setuptools in for allowing 'python setup.py develop' environment.

--- setup.py
+++ setup.py
@@ -1,30 +1,7 @@
-from distutils.core import setup, Extension
-import os
-
-def is_package(path):
-    return (
-        os.path.isdir(path) and
-        os.path.isfile(os.path.join(path, '__init__.py'))
-        )
-
-def find_packages(path, base="" ):
-    """ Find all packages in path """
-    packages = {}
-    for item in os.listdir(path):
-        dir = os.path.join(path, item)
-        if is_package( dir ):
-            if base:
-                module_name = "%(base)s.%(item)s" % vars()
-            else:
-                module_name = item
-            packages[module_name] = dir
-            packages.update(find_packages(dir, module_name))
-    return packages
-
-packages = find_packages("src")
+from setuptools import setup
 
 setup(name='qao',
       version='1.0',
-      package_dir = packages,
-      packages    = packages.keys(),
+      packages=['qao'],
+      package_dir={'':'src'},
      )
