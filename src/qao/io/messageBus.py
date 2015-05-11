1b9784a0ea1df6e221f5b934b235e5b966de2b4f
Tobias Lausch <lausch@rhrk.uni-kl.de>
2015-05-11 21:32:53 +0200

    Tested Messagebus PubSub.

--- src/qao/io/messageBus.py
+++ src/qao/io/messageBus.py
@@ -111,6 +111,7 @@ def simplePublish(topic, data, hostname, port = DEFAULT_PORT):
     c.publishEvent(topic, data)
     c.waitForEventPublished()
 
+
 class MessageBusCommunicator(QtCore.QObject):
     def __init__(self, masking=False):
         QtCore.QObject.__init__(self)
@@ -124,23 +125,25 @@ class MessageBusCommunicator(QtCore.QObject):
         self.handshakeDone = False
         self.incompleteData = ''
 
-    def _send(self,RawData, blocking=False):
+    def _send(self, rawData, blocking=False):
+        if not isinstance(rawData, bytes):
+            rawData = bytes(rawData.encode())
         stream = QtCore.QDataStream(self.connection)
         if blocking:
             while self.connection.bytesToWrite() > 0:
                 self.connection.waitForBytesWritten(DEFAULT_TIMEOUT)
-        return stream.writeRawData(RawData)
+        return stream.writeRawData(rawData)
 
-    def _sendFrame_(self,data, opCode):
+    def _sendFrame_(self, data, opCode):
         mask = os.urandom(4) if self.masking else None
-        frm = websocket.Frame(opCode,data,mask=mask,fin=1)
+        frm = websocket.Frame(opCode, data, mask=mask, fin=1)
         nWritten = self._send(frm.build())
 
-    def _sendPacket(self, data, binary = False):
+    def _sendPacket(self, data, binary=False):
         if len(data) <= 0: return
         opCode = websocket.OPCODE_BINARY if binary else websocket.OPCODE_ASCII
         dataSer = jsonEncoder.dumps(data, separators=(',', ':'), sort_keys=True)
-        self._sendFrame_(dataSer,opCode)
+        self._sendFrame_(dataSer, opCode)
 
     def _handleReadyRead(self):
         while self.connection.bytesAvailable() > 0:
@@ -159,7 +162,8 @@ class MessageBusCommunicator(QtCore.QObject):
                         break
             else:
                 try:
-                    if self.connection.bytesAvailable() < self.neededBytes: return
+                    if self.connection.bytesAvailable() < self.neededBytes:
+                        return
                     self.neededBytes = self.currentFrame.parser.send(str(self.connection.read(self.neededBytes)))
                 except StopIteration:
                     self._handleFrame_(self.currentFrame)
@@ -167,12 +171,12 @@ class MessageBusCommunicator(QtCore.QObject):
                     self.neededBytes = next(self.currentFrame.parser)
 
     def _pong_(self):
-        self._sendFrame_('',websocket.OPCODE_PONG)
+        self._sendFrame_('', websocket.OPCODE_PONG)
 
     def ping(self):
-        self._sendFrame_('',websocket.OPCODE_PING)
+        self._sendFrame_('', websocket.OPCODE_PING)
 
-    def _handleFrame_(self,frm):
+    def _handleFrame_(self, frm):
         if frm.opCode == websocket.OPCODE_CLOSE:
             print("Disconnect Packet received. Cleaning up Communicator and wait for disconnect.")
             self._cleanupCommunicator_()
@@ -183,7 +187,7 @@ class MessageBusCommunicator(QtCore.QObject):
             return
 
         if frm.opCode == websocket.OPCODE_ASCII or frm.opCode == websocket.OPCODE_BINARY:
-            self.incompleteData = "%s%s"%(self.incompleteData, frm.data)
+            self.incompleteData = "%s%s" % (self.incompleteData, frm.data)
             if frm.fin == 1:
                 self._handleNewPacket(self.incompleteData)
                 self.incompleteData = ''
@@ -262,7 +266,7 @@ class MessageBusClient(MessageBusCommunicator):
         """
         return self.connection.state() == self.connection.ConnectedState
 
-    def subscribe(self, topic, callback = None):
+    def subscribe(self, topic, callback=None):
         """
         Subscribe to a topic on the currently connected bus.
 
@@ -276,7 +280,8 @@ class MessageBusClient(MessageBusCommunicator):
         """
         topic = str(topic)
         self._sendPacket([TYPE_SUBSCRIBE, topic])
-        if callback: self.subscriptionCallbacks[topic] = callback
+        if callback:
+            self.subscriptionCallbacks[topic] = callback
 
     def unsubscribe(self, topic):
         """
@@ -361,7 +366,7 @@ class MessageBusClient(MessageBusCommunicator):
 
     def _sendHeader(self):
         hdr = websocket.DefaultHTTPClientHeader()
-        nWritten = self._send(hdr.createHeader(),blocking=True)
+        nWritten = self._send(hdr.createHeader(), blocking=True)
 
     def handleEvent(self, topic, data):
         self.receivedEvent.emit(topic, data)
@@ -379,7 +384,7 @@ class MessageBusClient(MessageBusCommunicator):
                 data.update({'error': str(e)})
             self._sendPacket([TYPE_RPC_REPLY, funcName, data])
 
-    def _handleHeaderReceived(self,httpHeader):
+    def _handleHeaderReceived(self, httpHeader):
         #TODO: check header received from the server
         self.connected.emit()
         pass
@@ -460,6 +465,7 @@ class ServerClientConnection(MessageBusCommunicator):
 
     def _handleNewPacket(self, dataRaw):
         try:
+            #TODO: decoding the whole data on server side should not be necessary.
             data = jsonEncoder.loads(dataRaw)
 
             if len(data) < 2:
@@ -544,7 +550,7 @@ class MessageBusServer(QtCore.QObject):
     clientDisconnected = qtSignal(object)
     eventPublished = qtSignal(str, object)
 
-    def __init__(self, port = DEFAULT_PORT):
+    def __init__(self, port=DEFAULT_PORT):
         QtCore.QObject.__init__(self)
         # setup server
         self.server = QtNetwork.QTcpServer()
@@ -562,7 +568,7 @@ class MessageBusServer(QtCore.QObject):
         self.clients.append(client)
         self.clientConnected.emit(client)
         print("new client connected (active connections: %d)" % len(self.clients))
-        assert (not self.server.hasPendingConnections()) # TODO: do we have to check for multiple connections?
+        assert (not self.server.hasPendingConnections())  # TODO: do we have to check for multiple connections?
 
     def _handlePublish(self, topic, data):
         topic = str(topic)
--- test/messagBus/TestMessageBus.py
+++ test/messagBus/TestMessageBus.py
@@ -5,21 +5,67 @@ Try to ensure Messagebus funtionality by creating a mocking
 messageBusCommunicator which maps Server send to Client read methods.
 """
 import unittest
-from qao.io.messageBus import MessageBusClient, MessageBusServer, MessageBusCommunicator
+import time
+from qao.gui.qt import QtCore
+from qao.io.messageBus import MessageBusClient, MessageBusServer
 
+app = QtCore.QCoreApplication([])
+
+TESTPORT = 12345
 
 class TestMessageBus(unittest.TestCase):
-    def test(self):
+
+    server = None
+
+    def setUp(self):
+        self.server = MessageBusServer(port=TESTPORT)
+        self.messageBus = MessageBusClient()
+        self.messageBus.connectToServer("localhost", TESTPORT)
+        self.process()
+        self.assertEqual(len(self.server.clients), 1, "At least one client should be connected")
+        self.assertTrue(self.messageBus.isConnected(), "MessageBus is not connected")
+
+    def tearDown(self):
+        self.messageBus.disconnectFromServer()
+        self.messageBus = None
+        del self.server.server
+        del self.server
+        self.server = None
+
+    def process(self):
+        for i in range(10):
+            app.processEvents()
+            self.messageBus.connection.waitForBytesWritten()
+            for client in self.server.clients:
+                client.connection.waitForBytesWritten()
+            time.sleep(0.01)
+            app.processEvents()
+
+
+    def testPublish(self):
+        # Assert
+        self.messageBus.publishEvent('topic', [1]*20)
+
+    def testSubscribe(self):
         """
-        
+        Ensure that subscription callback is handled correctly
         """
         # Arrange
-
-        # Act
+        expected = ["This", "is", "just", 1, "Test"]
 
         # Assert
-        self.assertEqual(True, False)
+        def assertion(result):
+            setattr(assertion, 'wasCalled', True)
+            self.assertListEqual(result, expected, "Unexpected: %s != %s" % (result, expected))
+        setattr(assertion, 'wasCalled', False)
 
+        # Act
+        self.messageBus.subscribe('topic', assertion)
+        self.process()
+        self.messageBus.publishEvent('topic', expected)
+        self.process()
+
+        self.assertTrue(assertion.wasCalled, "Assertion has not been called")
 
 if __name__ == '__main__':
     unittest.main()
