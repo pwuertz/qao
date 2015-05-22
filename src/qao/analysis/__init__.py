2cc59555b2925e3db7a0e13ac87bc05ff24e94e5
Thomas Niederprüm <niederp@physik.uni-kl.de>
2015-05-22 11:09:33 +0200

    analysis: set automodule trigger for the new correlations module

--- src/qao/analysis/__init__.py
+++ src/qao/analysis/__init__.py
@@ -4,6 +4,7 @@ High level data analysis
 
 .. automodule:: qao.analysis.bandstruct
 .. automodule:: qao.analysis.basex
+.. automodule:: qao.analysis.correlations
 .. automodule:: qao.analysis.image
 .. automodule:: qao.analysis.lattice2d
 .. automodule:: qao.analysis.prob_dist
